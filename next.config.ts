57579f06c387ccd76c5a7eaf200eee7817568ccc
daniil_linevych21 <dan4224rit@gmail.com>
2025-12-08 15:21:23 +0200

    Initial project commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: ['./src/styles'],
+    prependData: `@import "@/styles/variables.scss"; @import "@/styles/mixins.scss";`,
+  },
 };
 
 export default nextConfig;
